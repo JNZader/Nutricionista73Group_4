af36e21b34c778284bddb9040afee6840a10b7c1
pablobrizuela23 <latrell_302@hotmail.com>
2023-10-29 23:54:33 -0300

    anda modif

--- src/Vistas/ViewDieta.java
+++ src/Vistas/ViewDieta.java
@@ -868,6 +868,7 @@ public class ViewDieta extends javax.swing.JPanel {
     }//GEN-LAST:event_jButtonModifActionPerformed
 
     private void jbModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbModificarActionPerformed
+        actualizarTablaDos();
         try {
             DietaComidaDAO comidaDie = new DietaComidaDAO();
             DietaDAO dietaDAO = new DietaDAO();
@@ -890,8 +891,24 @@ public class ViewDieta extends javax.swing.JPanel {
                 comidaDie.actualizar(dietaComida);
 
 
-                llenarTablaDetalleDieta(die);
+                llenarTablaDetalleDieta(dietaComida.getDieta());
 
+            }else{
+                //DietaComida dietaComida = (DietaComida) jTablaDetalleDieta.getValueAt(selectedRow, 4);
+                DietaComida dietaComida; 
+                // Realizar la actualización
+                int porcion = Integer.parseInt(jtfPorcion.getText());
+                Comida comidas = (Comida) jComboBoxComidas.getSelectedItem();
+                Horario horarios = (Horario) jComboBoxHorario.getSelectedItem();
+                dietaComida = new DietaComida(dietCom.getId(), comidas,dietCom.getDieta(), porcion, horarios, true);
+                dietaComida.setPorcion(porcion);
+                dietaComida.setComida(comidas);
+                dietaComida.setHorario(horarios);
+
+                comidaDie.actualizar(dietaComida);
+
+
+                llenarTablaDetalleDieta(dietaComida.getDieta());
             }
         } catch (NumberFormatException e) {
             e.printStackTrace(System.out);
